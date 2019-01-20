57dfd7808997b3dc6593b239056db190ac97ff8e
fuadajip <fuadajipratomo@gmail.com>
2019-01-20 14:48:59 +0700

    [ADD] initial webpack babel configuration

---
+++ demo/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+import TestComponent from "../../src";
+
+const App = () => {
+  return <TestComponent />;
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+const TestComponent = () => {
+  return (
+    <div>
+      <h1>Hello component 2</h1>
+    </div>
+  );
+};
+
+export default TestComponent;
